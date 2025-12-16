e57703522540a7afe8828c8e4bfff54d7906bbbc
j-moreno-c-r <joaomcr@proton.me>
2025-12-15 22:17:07 -0300

    pool its coming

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "C development shell with GCC 11+";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+ 
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.gcc11
+            pkgs.gdb
+            pkgs.make
+            pkgs.pkg-config
+          ];
+          shellHook = ''
+            echo "Welcome, my brother, to your C dev shell with GCC >=11!"
+            gcc --version
+          '';
+        };
+      }
+    );
+}
---
+++ playlist_from_yt/n1.c
@@ -0,0 +1,7 @@
+int calc(int price){
+    return 10*price/100;
+}
+
+int main(){
+    return calc_disctount(100);
+}
